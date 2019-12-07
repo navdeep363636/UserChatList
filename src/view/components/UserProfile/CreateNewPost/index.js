070c905be76ad2eca12b845b54f2851cc987d738
razatverma <razatverma1994@gmail.com>
2019-12-07 11:45:34 +0530

    updates

--- src/view/components/UserProfile/CreateNewPost/index.js
+++ src/view/components/UserProfile/CreateNewPost/index.js
@@ -7,20 +7,6 @@ const userImg =
   "https://www.thefamouspeople.com/profiles/images/robert-downey-jr--2.jpg";
 export default () => (
   <div className="createNewPost">
-    <div className="createPostBtn">
-      <button>
-        <i class="fas fa-pencil-alt"></i>Create post
-      </button>
-      <button>
-        <i class="fas fa-camera"></i>Photo/Video
-      </button>
-      <button>
-        <i class="fas fa-video"></i>Live video
-      </button>
-      <button>
-        <i class="fas fa-flag"></i>Life Event
-      </button>
-    </div>
     <div className="tab">
       <TextPostab />
     </div>
