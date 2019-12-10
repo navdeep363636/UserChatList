2e6ec13aa92ab86fe343b02655f9aebc0261eb13
razatverma <razatverma1994@gmail.com>
2019-12-10 22:00:45 +0530

    updates

--- src/view/components/UserProfile/CreateNewPost/index.js
+++ src/view/components/UserProfile/CreateNewPost/index.js
@@ -4,35 +4,37 @@ import "./style.scss";
 import TextPostab from "./TextPostTab";
 
 export default class CreateNewPost extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { postText: "" };
-	}
-	setPostText = val => {
-		this.setState({ postText: val });
-	};
-	render() {
-		console.log("this.props", this.props);
-		const { addPost } = this.props;
-		const { postText } = this.state;
-		return (
-			<div className="createNewPost">
-				<div className="createPostBtn">
-					<button onClick={() => addPost(postText)}>Create Post</button>
-				</div>
-				<div className="tab">
-					<TextPostab postText={postText} setPostText={this.setPostText} />
-				</div>
-				<button
-					style={{
-						background: "#0099ff",
-						color: "#fff",
-						padding: "6px 12px",
-						borderRadius: 4
-					}}>
-					Post
-				</button>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = { postText: "" };
+  }
+  setPostText = val => {
+    this.setState({ postText: val });
+  };
+  render() {
+    console.log("this.props", this.props);
+    const { addPost } = this.props;
+    const { postText } = this.state;
+    return (
+      <div className="createNewPost">
+        <div className="createPostBtn">
+          <button onClick={() => addPost(postText)}>Create Post</button>
+        </div>
+        <div className="tab">
+          <TextPostab postText={postText} setPostText={this.setPostText} />
+          <button
+            style={{
+              background: "#0099ff",
+              color: "#fff",
+              padding: "6px 12px",
+              borderRadius: 4,
+              border: 0
+            }}
+          >
+            Post
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
