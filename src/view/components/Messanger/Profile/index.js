8143fd903f567d9dbc072e1304ac4a098b82f475
razatverma <razatverma1994@gmail.com>
2019-12-14 15:29:05 +0530

    updates

--- src/view/components/Messanger/Profile/index.js
+++ src/view/components/Messanger/Profile/index.js
@@ -1,3 +1,28 @@
 import React from "react";
+import styled, { css } from "styled-components";
+export default () => (
+  <div className="text-center">
+    <Profile src="https://www.thefamouspeople.com/profiles/images/robert-downey-jr--2.jpg" />
+    <Name>Razat Verma</Name>
+    <TimeAgo>Active 1H ago</TimeAgo>
+  </div>
+);
 
-export default () => <div></div>;
+const Profile = styled("img")`
+  width: 1em;
+  height: 1em;
+  font-size: 100px;
+  border-radius: 100%;
+  object-fit: cover;
+  margin-right: 12px;
+`;
+
+const Name = styled("h2")`
+  margin-top: 12px;
+  margin-bottom: 0;
+`;
+
+const TimeAgo = styled("p")`
+  font-size: 14px;
+  margin: 0;
+`;
